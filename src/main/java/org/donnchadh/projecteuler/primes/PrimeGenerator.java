f19dca7f0542d82e988407019b6ec644a16cc565
donnchadh <donnchadh@donnchadh>
2011-01-25 17:11:04 +0000

    Solution to Problem 2

--- src/main/java/org/donnchadh/projecteuler/primes/PrimeGenerator.java
+++ src/main/java/org/donnchadh/projecteuler/primes/PrimeGenerator.java
@@ -1,7 +1,21 @@
 package org.donnchadh.projecteuler.primes;
-class PrimeGenerator {
-    public static java.util.Iterator<Long> primes() {
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+public class PrimeGenerator implements Iterable<Long> {
+    final List<Long> primes = new ArrayList<Long>();
+    
+    private PrimeGenerator() {
+    }
+    
+    public java.util.Iterator<Long> primes() {
         return new java.util.Iterator<Long>() {
+            
+            private long lastPrime = 1;
 
             @Override
             public boolean hasNext() {
@@ -10,8 +24,27 @@ class PrimeGenerator {
 
             @Override
             public Long next() {
-                // TODO Auto-generated method stub
-                return null;
+                long next = lastPrime;
+                while (!isPrime(++next)) {
+                    ;
+                }
+                lastPrime = next;
+                Long result = Long.valueOf(lastPrime);
+                primes.add(result);
+                return result;
+            }
+
+            private boolean isPrime(long l) {
+                long midpoint = (long) Math.sqrt(l);
+                for (Long prime : primes) {
+                    if ((l % prime.longValue()) == 0) {
+                        return false;
+                    }
+                    if (prime.longValue() > midpoint) {
+                        return true;
+                    }
+                }
+                return true;
             }
 
             @Override
@@ -20,5 +53,20 @@ class PrimeGenerator {
             }
             
         };
+    }
+
+    public static PrimeGenerator instance() {
+        return new PrimeGenerator();
+    }
+
+    @Override
+    public Iterator<Long> iterator() {
+        return primes();
+    }
+    
+    public static void main(String[] args) {
+        for (Long prime : instance()) {
+            System.out.println(prime);
+        }
     }
 }
--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p000x/Problem2.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p000x/Problem2.java
@@ -1,5 +1,28 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p000x;
 
-public class Problem2 {
+import org.donnchadh.projecteuler.primes.PrimeGenerator;
 
+public class Problem2 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long remainingNumber = 600851475143L;
+        long midpoint = (long) Math.sqrt(remainingNumber);
+        long primeFactor = 1L;
+        for (Long prime : PrimeGenerator.instance()) {
+            if (prime.longValue() > midpoint)
+            {
+                break;
+            }
+
+            while ((remainingNumber % prime.longValue()) == 0)
+            {
+                System.out.println(primeFactor);
+                primeFactor = prime.longValue();
+                remainingNumber /= prime.longValue();
+                midpoint = (long)Math.sqrt(remainingNumber);
+            }
+        }
+        System.out.println(remainingNumber);
+        System.out.println("  ["+ (System.currentTimeMillis() - startTime) +" ms]");
+    }
 }
