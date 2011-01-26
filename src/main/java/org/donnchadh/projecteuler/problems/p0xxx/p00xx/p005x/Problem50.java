c88f935723d7657f0871cbc78b0a14ab57c0d2ef
donnchadh <donnchadh@donnchadh>
2011-01-26 11:06:25 +0000

    faster solution

--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p005x/Problem50.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p005x/Problem50.java
@@ -1,5 +1,6 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p005x;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,62 +12,59 @@ public class Problem50 {
     long lastPrime = 2;
     int lastCount = 0;
 
-    public long findSolution() {
-        long max = 1000000L;
+    public long findSolution(long max) {
         PrimeGenerator instance = PrimeGenerator.instance();
+        List<Long> sumsList = new ArrayList<Long>();
+        long maxSum = 0;
         for (Long prime : instance) {
-            if (prime.longValue() > max) {
+            sumsList.add(Long.valueOf(maxSum));
+            if (maxSum > max) {
                 break;
             }
+            maxSum += prime.longValue();
         }
-        List<Long> primeList = instance.getPrimes();
-        long[] primes = new long[primeList.size()];
-        for (int i = 0; i < primes.length; i++) {
-            primes[i] = primeList.get(i).longValue();
+        long[] sums = new long[sumsList.size()];
+        for (int i = 0; i < sums.length; i++) {
+            sums[i] = sumsList.get(i).longValue();
         }
-        int firstPrimeIndex = 0;
-        int lastPrimeIndex = 0;
-        int firstPrimeIndexUsed = 0;
-        long sum = primes[firstPrimeIndex];
-        while (primes[firstPrimeIndex] < max) {
-            int nextCandidatePrimeIndex = lastPrimeIndex;
-            for (;;) {
-                long nextPrime = primes[++nextCandidatePrimeIndex];
-                if ((sum + nextPrime) > max) {
-                    break;
-                }
-                sum += nextPrime;
-                int count = nextCandidatePrimeIndex - firstPrimeIndex + 1;
-                if (instance.isPrime(sum) && count >= lastCount) {
-                    lastPrime = nextPrime;
-                    lastSum = sum;
-                    lastCount = count;
-                    firstPrime = primes[firstPrimeIndex];
-                    firstPrimeIndexUsed = firstPrimeIndex;
-                    lastPrimeIndex = nextCandidatePrimeIndex;
+        for (int c = 0; c < sums.length; c++) {
+            for (int i = 0; i <= c; i++) {
+                long left = sums[i];
+                long right = maxSum - sums[sums.length-(c-i)-1];
+                long value = maxSum - (left + right);
+                if (value < max && instance.isPrime(value)) {
+                    firstPrime = sums[i+1]-left;
+                    lastPrime = sums[sums.length-(c-i)-1]-sums[sums.length-(c-i)-2];
+                    lastCount = sums.length - c - 1;
+                    return value;
                 }
             }
-            firstPrimeIndex++;
-            sum = 0;
-            for (int i = firstPrimeIndex; i <= lastPrimeIndex; i++) {
-                sum += primes[i];
-            }
-        }   
-        long sum2 = 0;
-        for (int i = firstPrimeIndexUsed; i <= lastPrimeIndex; i++) {
-            sum2 += primes[i];
         }
-        if (sum2 != lastSum) {
-            System.out.println("Error " + sum2 + " != " + lastSum);
-        }
-        return lastSum;
+        return 0;
     }
     
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         Problem50 problem50 = new Problem50();
-        long solution = problem50.findSolution();
+        long solution = problem50.findSolution(1000000L);
         System.out.println(solution + "("+problem50.firstPrime+" .. " + problem50.lastPrime + " "+problem50.lastCount +")");
         System.out.println("  ["+ (System.currentTimeMillis() - startTime) +" ms]");
+        long sum = 0;
+        int count = 0;
+        for (Long prime : PrimeGenerator.instance()) {
+            if (prime.longValue() > problem50.lastPrime) {
+                break;
+            }
+            if (prime.longValue() >= problem50.firstPrime) {
+                sum += prime.longValue();
+                count++;
+            }
+        }
+        if (sum != solution) {
+            System.out.println("Incorrect solution.");
+        }
+        if (count != problem50.lastCount) {
+            System.out.println("Incorrect count (should be: " + count + ")");
+        }
     }
 }
