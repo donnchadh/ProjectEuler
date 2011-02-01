170621976a57beaf81c2f4ad64110477c63e98be
donnchadh <donnchadh@ailefroide>
2011-02-01 23:07:57 +0000

    Problem 26

--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p002x/Problem26.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p002x/Problem26.java
@@ -1,15 +1,38 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p002x;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Problem26 {
     public static void main(String[] args) {
-        BigDecimal one = new BigDecimal(1.0, MathContext.UNLIMITED);
+        int dForMaxK = 0;
+        int maxK = 0;
         for (int d = 3; d <1000; d++) {
-            BigDecimal bd = BigDecimal.ONE.divide(BigDecimal.valueOf(d));
-            System.out.println(bd.toString());
+            int a = 1;
+            Map<Integer, Integer> remainders = new HashMap<Integer, Integer>();
+            for (int n = 0; ; n++) {
+                a *= 10;
+                if (a > d) {
+                    a = a%d;
+                    Integer a_ = Integer.valueOf(a);
+                    if (a == 0) {
+                        break;
+                    }
+                    if (remainders.containsKey(a_)) {
+                        int period = n - remainders.get(a_).intValue();
+                        if (period > maxK) {
+                            maxK = period;
+                            dForMaxK = d;
+                        }
+                        System.out.print(".");
+                        break;
+                    }
+                    remainders.put(a_, n);
+                }
+            }
         }
+        System.out.println();
+        System.out.println(dForMaxK);
     }
 }
