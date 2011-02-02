3fcc42a19546c8765507b5c8bb34e57c6b1f0c3e
donnchadh <donnchadh@donnchadh>
2011-02-02 09:54:20 +0000

    More solutions

--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p004x/Problem41.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p004x/Problem41.java
@@ -1,6 +1,7 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p004x;
 
 import org.donnchadh.projecteuler.primes.PrimeGenerator;
+import org.donnchadh.projecteuler.utils.PanDigitals;
 
 public class Problem41 {
     int[] primeFinalDigits = {1,3,7,9};
@@ -8,27 +9,7 @@ public class Problem41 {
         PrimeGenerator primeGenerator = PrimeGenerator.instance();
         int max = 987654321;
         for (int i = max; i > 0; i-=2) {
-            int mask = 0;
-            int x = i;
-            boolean panDigital = true;
-            int j;
-            for (j = 0; j <9 && x > 0; j++) {
-                if ((x%10) != 0) {
-                    int maskBefore = mask;
-                    mask |= (1 << (x%10));
-                    if (maskBefore == mask) {
-                        panDigital = false;
-                        break;
-                    }
-                } else {
-                    panDigital = false;
-                    break;
-                }
-                x /= 10;
-            }
-            if (mask != (1<<(j+1))-2) {
-                panDigital = false;
-            }
+            boolean panDigital = PanDigitals.isPanDigital(i);
             if (panDigital) {
                 if (primeGenerator.isPrime(i)) {
                     System.out.println("---");
