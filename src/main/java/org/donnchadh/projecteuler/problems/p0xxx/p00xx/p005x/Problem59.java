2e771339b1377ee74b04dd3a2d8669e7a40a1b4c
donnchadh <donnchadh@ailefroide>
2011-02-11 23:50:19 +0000

    Solution 63

---
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p005x/Problem59.java
@@ -0,0 +1,7 @@
+package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p005x;
+
+public class Problem59 {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p006x/Problem63.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p00xx/p006x/Problem63.java
@@ -1,7 +1,27 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p00xx.p006x;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem63 {
     public static void main(String[] args) {
-        
+        int count = 0;
+        Set<BigInteger> integers = new HashSet<BigInteger>();
+        for (int n = 0; n <= 10; n++) {
+            for (int p = 0; p < 30; p++) {
+                BigInteger y = BigInteger.valueOf(n).pow(p);
+                int c = 0;
+                while (y.bitLength() > 0) {
+                    y = y.divide(BigInteger.TEN);
+                    c++;
+                }
+                if (c == p) {
+                    count++;
+                    integers.add(BigInteger.valueOf(n).pow(p));
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
