96f4737a562af17455aedcc9a6fca707dbfc40c5
donnchadh <donnchadh@ailefroide>
2011-02-14 23:17:57 +0000

    Problem 124

---
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p01xx/p012x/Problem124.java
@@ -0,0 +1,15 @@
+package org.donnchadh.projecteuler.problems.p0xxx.p01xx.p012x;
+
+public class Problem124 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; count <= 10000; i++) {
+            int[] primeFactors = factorize(i);
+        }
+    }
+
+    private static int[] factorize(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
