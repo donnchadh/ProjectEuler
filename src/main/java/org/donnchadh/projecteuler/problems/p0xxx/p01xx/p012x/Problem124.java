dee202caefc6eac818f3deaf0ae88ba4d0ec6472
donnchadh <donnchadh@donnchadh>
2011-02-15 09:27:39 +0000

    Outline of solution to problem 124

--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p01xx/p012x/Problem124.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p01xx/p012x/Problem124.java
@@ -1,5 +1,7 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p01xx.p012x;
 
+import java.util.Arrays;
+
 /**
  * @see http://projecteuler.net/index.php?section=problems&id=124
  * 
@@ -10,21 +12,25 @@ public class Problem124 {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; count <= 10000; i++) {
-            int[] primeFactors = factorize(i);
-            int d = combinationsLessThan(primeFactors, 100000);
-            if (count + d >= 10000) {
+            int[] primeFactors = factorizeIfRadical(i);
+            int[] c = combinationsLessThan(primeFactors, 100000);
+            if (count + c.length >= 10000) {
                 // sort combinations to get solution = (count + d - 10000th) product of prime factors
+                Arrays.sort(c);
+                System.out.println(c[count + c.length - 10000]);
+            } else {
+                count += c.length;
             }
         }
     }
 
-    private static int combinationsLessThan(int[] primeFactors, int limit) {
+    private static int[] combinationsLessThan(int[] primeFactors, int limit) {
         // TODO Auto-generated method stub
-        return 0;
+        return new int[] {};
     }
 
-    private static int[] factorize(int i) {
+    private static int[] factorizeIfRadical(int i) {
         // TODO Auto-generated method stub
-        return null;
+        return new int[] {};
     }
 }
