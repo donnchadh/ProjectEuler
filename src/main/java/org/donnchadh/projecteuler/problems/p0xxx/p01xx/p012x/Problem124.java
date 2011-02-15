105da0f29b742a0bbd2b7bec9653fb192d4d162f
donnchadh <donnchadh@donnchadh>
2011-02-15 09:20:08 +0000

    Problem 124

--- src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p01xx/p012x/Problem124.java
+++ src/main/java/org/donnchadh/projecteuler/problems/p0xxx/p01xx/p012x/Problem124.java
@@ -1,5 +1,11 @@
 package org.donnchadh.projecteuler.problems.p0xxx.p01xx.p012x;
 
+/**
+ * @see http://projecteuler.net/index.php?section=problems&id=124
+ * 
+ * @author donnchadh
+ *
+ */
 public class Problem124 {
     public static void main(String[] args) {
         int count = 0;
@@ -10,6 +16,11 @@ public class Problem124 {
                 // sort combinations to get solution = (count + d - 10000th) product of prime factors
             }
         }
+    }
+
+    private static int combinationsLessThan(int[] primeFactors, int limit) {
+        // TODO Auto-generated method stub
+        return 0;
     }
 
     private static int[] factorize(int i) {
